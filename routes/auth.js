3dc7635921c85fa2a9776ad82e7d80f2859d2b8d
Chirag2001op <chirag.dogra79@gmail.com>
2022-01-27 02:18:00 +0530

    saving user data in database

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const User = require('../models/User');
 const router = express.Router();
 
-router.get('/',(req, res)=>{
-    obj = {
-        a: 'thios',
-        number:34
-    }
-    res.json(obj)
+
+
+
+//Create a User using: POST '/api/auth/'. Doesn't require Auth
+router.post('/',(req, res)=>{
+    console.log(req.body) //if we want to use req.body we have to use app.use(express.json()) middleware in index.js  
+    const user = User(req.body); 
+    user.save();
+    res.send(req.body)
 })
-module.exports = router;
+module.exports = router; 
