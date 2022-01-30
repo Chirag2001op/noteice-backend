eb4beb661a6b55e9c527a275e5eb0cefc3f1b802
Chirag2001op <chirag.dogra79@gmail.com>
2022-01-31 01:48:58 +0530

    implemented bcryptjs and JWT

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,10 @@ const express = require("express");
 const User = require("../models/User");
 const router = express.Router();
 const { body, validationResult } = require("express-validator");
+const bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken')
 
+const JWT_SECRET = 'ChinuIsGreat$'
 //Create a User using: POST '/api/auth/createUser'. No login required
 router.post(
   "/createUser",
@@ -26,16 +29,28 @@ router.post(
           .status(400)
           .json({ error: "Sorry a user with this email already exixts" });
       }
+      const salt = await bcrypt.genSalt(10);
+      const secPass = await bcrypt.hash(req.body.password, salt) ;
       //creating new user
       user = await User.create({
         name: req.body.name,
-        password: req.body.password,
+        password: secPass,
         email: req.body.email,
       });
       // .then(user=> res.json(user))
       // .catch(err=>{console.log(err)
       // res.json({error: 'Please enter a unique value for email', message: err.message})}); //Commenting out this beause if there is any other error than email then we can send this error message
-      res.json(user);
+      
+      const data = {
+        user:{
+          id: user.id
+        }
+      }
+      const authToken = jwt.sign(data, JWT_SECRET);
+      console.log(authToken);
+
+      res.json({authToken})
+      // res.json(user);  //we are not sending user anymore we will be sending authToken now
     } catch (error) {
       console.error(error.meassage);
       res.status(500).send("Some Error occured");
