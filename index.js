065cc7a4ac5c6e0cbd9a8973297804fd774788b1
Chirag2001op <chirag.dogra79@gmail.com>
2022-02-24 12:56:46 +0530

    Delete request is not working

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const connectToMongo = require('./db')
+const express = require('express')
+var cors = require('cors')
 
 connectToMongo();
 
-const express = require('express')
 const app = express()
 const port = 5000
+
+app.use(cors())
 
 app.use(express.json())
 
@@ -13,5 +16,5 @@ app.use('/api/notes', require('./routes/notes'))
 
 
 app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`)
+  console.log(`Noteice backend listening on port http://localhost:${port}`)
 })
